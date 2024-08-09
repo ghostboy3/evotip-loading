d436c8ac8e2b2a941f3a8a0d82f9dc012b934c45
ghostboy3 <nico.luu.to@gmail.com>
2024-08-08 22:31:13 -0400

    turn led on

---
+++ testing.py
@@ -0,0 +1,11 @@
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BCM)
+led_pin = 18
+GPIO.setup(led_pin, GPIO.OUT)
+
+GPIO.output(led_pin, GPIO.HIGH)
+time.sleep(5)
+GPIO.output(led_pin, GPIO.LOW)
+GPIO.cleanup()
